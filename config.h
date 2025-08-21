b5d661cf235402893bf9483172e4029faad67558
Dave Esplana <daveesplana@outloook.com>
2025-08-21 09:56:15 +0800

    Updated configuration file

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Roboto Mono:size=11" };
+static const char dmenufont[]       = "Roboto Mono:size=11";
 static const char col_gray1[]       = "#111111";
 static const char col_gray2[]       = "#1A4D2E";
 static const char col_gray3[]       = "#bbbbbb";
@@ -57,8 +57,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0";
-static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *slock[]    = { "slock", NULL };
 static const char *upvol[]    = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
@@ -67,7 +67,7 @@ static const char *mutevol[]  = { "/usr/bin/pactl", "set-sink-mute", "@DEFAULT_S
 static const char *brightup[] = { "brightnessctl", "set", "+2%", NULL };
 static const char *brightdn[] = { "brightnessctl", "set", "2%-", NULL };
 static const char *clipmenu[] = { "clipmenu", NULL };
-static const char *browser[]  = { "brave-browser", NULL };
+static const char *browser[]  = { "librewolf", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -94,12 +94,15 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-        { 0,                            XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
-	{ 0,                            XF86XK_AudioMute,        spawn, {.v = mutevol } },
-	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
-	{ MODKEY,                       XK_F6,     spawn,          {.v = brightup} },
-        { MODKEY,                       XK_F5,     spawn,          {.v = brightdn} },
+        { MODKEY,                       XK_F9,     spawn,          {.v = downvol } },
+	{ MODKEY,                       XK_F8,     spawn,          {.v = mutevol } },
+	{ MODKEY,                       XK_F10,    spawn,          {.v = upvol   } },
+	{ MODKEY,                       XK_F7,     spawn,          {.v = brightup} },
+        { MODKEY,                       XK_F6,     spawn,          {.v = brightdn} },
 	{ MODKEY,                       XK_Escape, spawn,          {.v = slock} },
+	{ 0,                            XF86XK_AudioRaiseVolume,    spawn,          {.v = upvol   } },
+	{ 0,                            XF86XK_AudioLowerVolume,     spawn,          {.v = downvol } },
+	{ 0,                            XF86XK_AudioMute,     spawn,          {.v = mutevol } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = clipmenu} },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = browser } },
 	TAGKEYS(                        XK_1,                      0)
